851e0268b0df8e8997025afb0e768d5e2cb3beec
Angelosewase <sewasejo8@gmail.com>
2025-08-14 21:06:19 +0200

    change the profile picture

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -171,7 +171,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/hero.jpg"
+              src="../../images/pro.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
